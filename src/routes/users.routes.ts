21c713306833f4d9e8c42e06a029843778fad9bc
Fabricio Salomon <fabriciosalomon14@gmail.com>
2022-10-25 11:15:13 -0300

    [HotFix]: Fix routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,14 +3,14 @@ import { usersController } from "../controllers";
 
 export const usersRoutes = Router();
 
-usersRoutes.get("/users", async (request, response) => {
+usersRoutes.get("/api/users", async (request, response) => {
   await usersController.getUsers(request, response);
 });
 
-usersRoutes.get("/users/:username/details", async (request, response) => {
+usersRoutes.get("/api/users/:username/details", async (request, response) => {
   await usersController.getUserDetails(request, response);
 });
 
-usersRoutes.get("/users/:username/repos", async (request, response) => {
+usersRoutes.get("/api/users/:username/repos", async (request, response) => {
   await usersController.getUserRepositories(request, response);
 });
