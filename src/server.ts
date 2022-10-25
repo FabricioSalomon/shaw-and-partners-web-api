b913b8d2fcbd79894c260c47db1d0e57daa6b59b
Fabricio Salomon <fabriciosalomon14@gmail.com>
2022-10-25 08:29:18 -0300

    [Hotfix]: Add cors and procfile

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { response } from "express";
 import { router } from "./routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(router);
 
-app.listen(3000, () => console.log("Server is running!"));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running!"));
